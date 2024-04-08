fd8c3c22083a39e97ae3fbc9f2c426c1d16261bc
bradzara <bradzara93@gmail.com>
2024-04-07 22:44:24 -0700

    First deliberate practice

--- practice_day1.rb
+++ practice_day1.rb
@@ -6,3 +6,16 @@ while number <= 10
   puts number
   number = number +1
 end
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "example"
+
+puts word.upcase
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+number1 = 5
+number2 = 7
+
+puts number1 + number2
