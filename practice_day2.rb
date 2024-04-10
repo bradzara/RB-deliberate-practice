301adb618ad9079b0d043dcef12813289f93da7b
bradzara <bradzara93@gmail.com>
2024-04-09 22:35:03 -0700

    practice day 2

---
+++ practice_day2.rb
@@ -0,0 +1,11 @@
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+float = 5.74
+puts float.to_i
+
+# Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+x = 3
+y = 7
+
+puts x * y
