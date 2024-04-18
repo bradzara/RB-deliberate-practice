128eb2ce65fb3033a0d13e0823da053244f77879
bradzara <bradzara93@gmail.com>
2024-04-18 10:39:03 -0700

    Practice day 7

---
+++ practice_day7.rb
@@ -0,0 +1,5 @@
+# Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+countries = ["United States of America", "Italy", "El Salvador"]
+countries << "Japan"
+pp countries
