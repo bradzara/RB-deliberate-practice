6b6a8b46adb5185a6bc47c07be0fdcf6daef1d2d
bradzara <bradzara93@gmail.com>
2024-05-23 14:02:52 -0700

    JS Practice day 2.1

---
+++ practice_day2.1.js
@@ -0,0 +1,33 @@
+// Write a while loop to print the numbers 1 through 10.
+
+var number = 1;
+
+while (number <= 10) {
+  console.log(number);
+  number++;
+}
+
+// Write a while loop that prints the word "hello" 5 times.
+
+var i = 0;
+while (i < 5) {
+  console.log("hello");
+  i++;
+}
+
+// Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+// while (true) {
+//   var input = window.prompt("Enter a word: ");
+//   if (input === "stop") {
+//     break;
+//   }
+// }
+
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+var number = 0;
+while (number <= 100) {
+  console.log(number);
+  number = number + 5;
+}
